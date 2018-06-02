eb943e09675e344d7588467be39ac72aa4b33ad4
yisojin <tat0402@naver.com>
2018-06-03 01:40:25 +0900

    카테고리 관련 클릭시 이벤트처리

---
+++ Assets/Script/Category/MainCategory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCategory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void TrainingClick(){
+        // 메인 카테고리에서 훈련을 클릭했을때
+
+    }
+    public void RestClick(){
+        // 메인 카테고리에서 훈련을 클릭했을때
+
+    }
+    public void StoreClick(){
+        // 메인 카테고리에서 훈련을 클릭했을때
+
+    }
+    public void JobClick(){
+        // 메인 카테고리에서 훈련을 클릭했을때
+
+    }
+    public void ContestClick(){
+        // 메인 카테고리에서 훈련을 클릭했을때
+
+    }
+}
---
+++ Assets/Script/Category/Rest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rest : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void EatClick(){
+        PlayerPrefs.SetInt("time", PlayerPrefs.GetInt("time") - 60);
+        PlayerPrefs.SetInt("fatigue", PlayerPrefs.GetInt("fatigue") - 7);
+    }
+    void SleepClick(){
+        PlayerPrefs.SetInt("time", PlayerPrefs.GetInt("time") - 420);
+        PlayerPrefs.SetInt("fatigue", PlayerPrefs.GetInt("fatigue") - 40);
+    }
+    void EnergyDrinkClick(){
+        PlayerPrefs.SetInt("time", PlayerPrefs.GetInt("time") - 5);
+        PlayerPrefs.SetInt("fatigue", PlayerPrefs.GetInt("fatigue") - 3);
+    }
+}
