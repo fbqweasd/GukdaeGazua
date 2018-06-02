79512bb5e75ef1f8f76357469fd8a3164315ec5a
JeongHanna <jhn3981@gmail.com>
2018-06-03 04:35:42 +0900

    레이아웃 수정 및 메인 스크립트 추가

---
+++ Assets/Script/Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+
+    public Text UsernameText;
+    public Text TimeText;
+
+    // Use this for initialization
+    void Start () {
+        UsernameText.text = PlayerPrefs.GetString("username");
+        TimeText.text = PlayerPrefs.GetInt("time") + "분";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
