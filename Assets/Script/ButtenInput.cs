ce31b4aa515d745ab0079acd3b4d72379d89ca46
fbqweasd <fbqweasd2911@gmail.com>
2018-06-03 08:34:49 +0900

    버튼 이벤트 추가

---
+++ Assets/Script/ButtenInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtenInput : MonoBehaviour {
+
+	public void TrainingButton()
+    {
+        SceneManager.LoadScene("Scenes/Training");
+    }
+
+    public void StoreButton()
+    {
+        SceneManager.LoadScene("Scenes/Store");
+    }
+
+    public void RestButton()
+    {
+        SceneManager.LoadScene("Scenes/Rest");
+    }
+
+    public void JobButton()
+    {
+        SceneManager.LoadScene("Scenes/Job");
+    }
+
+    public void ContestButton()
+    {
+        SceneManager.LoadScene("Scenes/Contest");
+    }
+}
