516c8b36cc7b1b358d1a35f5c1111584980a7cd9
fbqweasd <fbqweasd2911@gmail.com>
2018-06-03 03:28:43 +0900

    카테고리 복구

---
+++ Assets/Script/Category/Job.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Job : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void StoreOnClick(){
+        PlayerPrefs.SetInt("time", PlayerPrefs.GetInt("time") - 60);
+        PlayerPrefs.SetInt("money", PlayerPrefs.GetInt("money") + 7530);
+    }
+    public void TutoringOnClick(){
+        
+        PlayerPrefs.SetInt("time", PlayerPrefs.GetInt("time") - 60);
+        PlayerPrefs.SetInt("money", PlayerPrefs.GetInt("money") + 10000);
+    }
+}
