a2f19bb2f2eb22af160ef54065410c498d1de183
JeongHanna <jhn3981@gmail.com>
2018-06-02 22:59:28 +0900

    구조 수정

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public void PlayButtonOnCilck() {
+        Debug.Log("Play Button Click");
+        SceneManager.LoadScene("Scenes/Main");
+        PlayerPrefs.DeleteAll();
+    }
+
+    public void ContinueButtonOnCilck()
+    {
+        Debug.Log("Continue Button Click");
+    }
+
+    public void ViewButtonOnCilck()
+    {
+        Debug.Log("View Button Click");
+    }
+}
