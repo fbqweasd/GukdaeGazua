1b2f42ce22842118335ec413b04b38abf18ea658
fbqweasd <fbqweasd2911@gmail.com>
2018-06-02 21:45:38 +0900

    기본 test 파일

---
+++ Assets/Script/Button_Script.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_Script : MonoBehaviour {
+
+    public void Start_Bt_cilck() {
+        Debug.Log("Start_Bt_cilck");
+        SceneManager.LoadScene("Scene/Play_Form");
+    }
+
+    public void reStart_Bt_cilck()
+    {
+        Debug.Log("reStart");
+    }
+
+    public void view_player_Bt_cilck()
+    {
+        Debug.Log("view_player");
+    }
+
+    public void Exit_Bt()
+    {
+        SceneManager.LoadScene("Scene/main_Form");
+        //Application.Quit();
+    }
+}
