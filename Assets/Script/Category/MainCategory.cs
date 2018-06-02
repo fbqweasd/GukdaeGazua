f65895f18121a695ebf58ae47fca656983938f11
yisojin <tat0402@naver.com>
2018-06-03 07:01:21 +0900

    수정 (scenes )

--- Assets/Script/Category/MainCategory.cs
+++ Assets/Script/Category/MainCategory.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainCategory : MonoBehaviour {
 
@@ -15,22 +16,23 @@ public class MainCategory : MonoBehaviour {
 	}
     public void TrainingOnClick(){
         // 메인 카테고리에서 훈련을 클릭했을때
-
+        SceneManager.LoadScene("Scenes/Training");
     }
     public void RestOnClick(){
-        // 메인 카테고리에서 훈련을 클릭했을때
-
+        // 메인 카테고리에서 휴식을 클릭했을때
+        SceneManager.LoadScene("Scenes/Rest");
     }
     public void StoreOnClick(){
-        // 메인 카테고리에서 훈련을 클릭했을때
-
+        // 메인 카테고리에서 상점을 클릭했을때
+        SceneManager.LoadScene("Scenes/Store");
     }
     public void JobOnClick(){
-        // 메인 카테고리에서 훈련을 클릭했을때
-
+        // 메인 카테고리에서 알바를 클릭했을때
+        SceneManager.LoadScene("Scenes/Job");
     }
     public void ContestOnClick(){
-        // 메인 카테고리에서 훈련을 클릭했을때
+        // 메인 카테고리에서 대회를 클릭했을때
 
+        SceneManager.LoadScene("Scenes/Contest");
     }
 }
