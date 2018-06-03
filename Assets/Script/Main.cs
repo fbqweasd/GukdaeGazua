e9098a1430b5e372b0c8c1ebab623aca4e58dcc4
yisojin <tat0402@naver.com>
2018-06-03 10:07:54 +0900

    timer 1초당 1분 줄기

--- Assets/Script/Main.cs
+++ Assets/Script/Main.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Timers;
 
 public class Main : MonoBehaviour {
 
+    private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        PlayerPrefs.SetInt("time", PlayerPrefs.GetInt("time") - 1);
+    }
+
+
     public Text UsernameText;
     public Text TimeText;
+    public Timer timer;
 
     // Use this for initialization
     void Start () {
+
+        timer = new System.Timers.Timer(1000);
+        timer.Elapsed += Timer_Elapsed;
+        timer.Enabled = true;
         UsernameText.text = PlayerPrefs.GetString("username");
         TimeText.text = PlayerPrefs.GetInt("time") + "분";
 	}
